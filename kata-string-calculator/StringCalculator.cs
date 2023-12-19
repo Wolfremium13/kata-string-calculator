62f78a8196bba78a6fd6de4f0877902c82ce34fb
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-19 11:27:50 +0000

    ♻ extract method

--- kata-string-calculator/StringCalculator.cs
+++ kata-string-calculator/StringCalculator.cs
@@ -9,27 +9,21 @@ namespace kata_string_calculator
         public static int Add(string numbers)
         {
             if (numbers.Length == 0) return 0;
-            if (!numbers.StartsWith("//")) return SumNumbers(numbers, new List<string>());
-            if (numbers[2] == '[')
-            {
-                var delimiters = GetDelimiters(numbers);
-                var numbersWithoutDelimiter = numbers.Substring(numbers.IndexOf(']') + 2);
-                return SumNumbers(numbersWithoutDelimiter, delimiters);
-            }
-            else
-            {
-                var delimiter = numbers[2].ToString();
-                var numbersWithoutDelimiter = numbers.Substring(4);
-                return SumNumbers(numbersWithoutDelimiter, new List<string> { delimiter });
-            }
-
+            var delimiters = GetDelimiters(numbers);
+            var numbersWithoutDelimiter = RemoveDelimiters(numbers);
+            return SumNumbers(numbersWithoutDelimiter, delimiters);
         }
 
         private static int SumNumbers(string numbers, IEnumerable<string> customDelimiters)
         {
             var delimiters = new List<string> { ",", "\n" }.Concat(customDelimiters);
-            return numbers.Split(delimiters.ToArray(), System.StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+            return numbers.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
                 .Sum();
+        }
+
+        private static string RemoveDelimiters(string numbers)
+        {
+            return numbers.StartsWith("//") ? numbers.Substring(numbers.IndexOf('\n') + 1) : numbers;
         }
 
         private static IEnumerable<string> GetDelimiters(string numbers)
