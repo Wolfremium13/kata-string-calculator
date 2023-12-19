90651ea0abcc7a45d919524d76fb719fae0cc441
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-19 10:32:20 +0000

    ✅ sum_numbers

--- kata-string-calculator/StringCalculator.cs
+++ kata-string-calculator/StringCalculator.cs
@@ -6,6 +6,11 @@ namespace kata_string_calculator
         {
             if (numbers.Length > 0)
             {
+                if (numbers.Contains(","))
+                {
+                    var splitNumbers = numbers.Split(',');
+                    return int.Parse(splitNumbers[0]) + int.Parse(splitNumbers[1]);
+                }
                 return int.Parse(numbers);
             }
 
