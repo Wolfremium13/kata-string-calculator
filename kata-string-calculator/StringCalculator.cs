1af4ff1f08bdaeb80110770cfa7788e3904db5c8
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-19 11:06:09 +0000

    ✅ sum_using_custom_delimiters_of_any_length

--- kata-string-calculator/StringCalculator.cs
+++ kata-string-calculator/StringCalculator.cs
@@ -10,17 +10,28 @@ namespace kata_string_calculator
             if (numbers.Length == 0) return 0;
             if (numbers.StartsWith("//"))
             {
-                var delimiter = numbers[2];
-                var numbersWithoutDelimiter = numbers.Substring(4);
-                return SumNumbers(numbersWithoutDelimiter, new[] { delimiter });
+                if (numbers[2] == '[')
+                {
+                    var delimiter = numbers.Substring(3, numbers.IndexOf(']') - 3);
+                    var numbersWithoutDelimiter = numbers.Substring(numbers.IndexOf(']') + 2);
+                    return SumNumbers(numbersWithoutDelimiter, new[] { delimiter });
+                }
+                else
+                {
+                    var delimiter = numbers[2].ToString();
+                    var numbersWithoutDelimiter = numbers.Substring(4);
+                    return SumNumbers(numbersWithoutDelimiter, new[] { delimiter });
+                }
             }
-            return SumNumbers(numbers, new char[] { });
+
+            return SumNumbers(numbers, new string[] { });
         }
 
-        private static int SumNumbers(string numbers, IEnumerable<char> customDelimiters)
+        private static int SumNumbers(string numbers, string[] customDelimiters)
         {
-            var delimiters = new[] { ',', '\n' }.Concat(customDelimiters).ToArray();
-            return numbers.Split(delimiters).Aggregate(0, (a, b) => a + int.Parse(b));
+            var delimiters = new List<string> { ",", "\n" }.Concat(customDelimiters);
+            return numbers.Split(delimiters.ToArray(), System.StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                .Sum();
         }
     }
 }
