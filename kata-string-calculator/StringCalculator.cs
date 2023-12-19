3b7a1e22f889d2c378622f29f9246f69c3814bb0
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-19 10:18:40 +0000

    ❌ ignore_empty

---
+++ kata-string-calculator/StringCalculator.cs
@@ -0,0 +1,10 @@
+namespace kata_string_calculator
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            return -1;
+        }
+    }
+}
---
+++ kata-string-calculator/StringCalculatorShould.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace kata_string_calculator
+{
+    public class StringCalculatorShould
+    {
+        [Fact]
+        public void ignore_empty()
+        {
+            Assert.Equal(new StringCalculator().Add(""), 0);
+        }
+    }
+}
