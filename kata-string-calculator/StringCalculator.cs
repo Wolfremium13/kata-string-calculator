a5974022bc12db6d2e4e44cdf14690eb478261f3
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-19 11:21:20 +0000

    ♻ extract method

--- kata-string-calculator/StringCalculator.cs
+++ kata-string-calculator/StringCalculator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -5,33 +6,48 @@ namespace kata_string_calculator
 {
     public class StringCalculator
     {
-        public int Add(string numbers)
+        public static int Add(string numbers)
         {
             if (numbers.Length == 0) return 0;
-            if (numbers.StartsWith("//"))
+            if (!numbers.StartsWith("//")) return SumNumbers(numbers, new List<string>());
+            if (numbers[2] == '[')
             {
-                if (numbers[2] == '[')
-                {
-                    var delimiter = numbers.Substring(3, numbers.IndexOf(']') - 3);
-                    var numbersWithoutDelimiter = numbers.Substring(numbers.IndexOf(']') + 2);
-                    return SumNumbers(numbersWithoutDelimiter, new[] { delimiter });
-                }
-                else
-                {
-                    var delimiter = numbers[2].ToString();
-                    var numbersWithoutDelimiter = numbers.Substring(4);
-                    return SumNumbers(numbersWithoutDelimiter, new[] { delimiter });
-                }
+                var delimiters = GetDelimiters(numbers);
+                var numbersWithoutDelimiter = numbers.Substring(numbers.IndexOf(']') + 2);
+                return SumNumbers(numbersWithoutDelimiter, delimiters);
+            }
+            else
+            {
+                var delimiter = numbers[2].ToString();
+                var numbersWithoutDelimiter = numbers.Substring(4);
+                return SumNumbers(numbersWithoutDelimiter, new List<string> { delimiter });
             }
 
-            return SumNumbers(numbers, new string[] { });
         }
 
-        private static int SumNumbers(string numbers, string[] customDelimiters)
+        private static int SumNumbers(string numbers, IEnumerable<string> customDelimiters)
         {
             var delimiters = new List<string> { ",", "\n" }.Concat(customDelimiters);
             return numbers.Split(delimiters.ToArray(), System.StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
                 .Sum();
+        }
+
+        private static IEnumerable<string> GetDelimiters(string numbers)
+        {
+            var defaultDelimiters = new List<string>() { ",", "\n" };
+            if (!numbers.StartsWith("//")) return defaultDelimiters;
+            if (numbers[2] == '[')
+            {
+                var delimiter = numbers.Substring(3, numbers.IndexOf(']') - 3);
+                defaultDelimiters.Add(delimiter);
+            }
+            else
+            {
+                var delimiter = numbers[2].ToString();
+                defaultDelimiters.Add(delimiter);
+            }
+
+            return defaultDelimiters;
         }
     }
 }
--- kata-string-calculator/StringCalculatorShould.cs
+++ kata-string-calculator/StringCalculatorShould.cs
@@ -7,40 +7,40 @@ namespace kata_string_calculator
         [Fact]
         public void ignore_empty()
         {
-            Assert.Equal(new StringCalculator().Add(""), 0);
+            Assert.Equal(StringCalculator.Add(""), 0);
         }
 
         [Fact]
         public void give_the_same_number()
         {
-            Assert.Equal(new StringCalculator().Add("1"), 1);
-            Assert.Equal(new StringCalculator().Add("2"), 2);
+            Assert.Equal(StringCalculator.Add("1"), 1);
+            Assert.Equal(StringCalculator.Add("2"), 2);
         }
 
         [Fact]
         public void sum_numbers()
         {
-            Assert.Equal(new StringCalculator().Add("1,2"), 3);
-            Assert.Equal(new StringCalculator().Add("2,3,5"), 10);
+            Assert.Equal(StringCalculator.Add("1,2"), 3);
+            Assert.Equal(StringCalculator.Add("2,3,5"), 10);
         }
         
         [Fact]
         public void sum_using_new_lines()
         {
-            Assert.Equal(new StringCalculator().Add("2\n3\n5"), 10);
+            Assert.Equal(StringCalculator.Add("2\n3\n5"), 10);
         }
         
         [Fact]
         public void sum_using_custom_delimiters()
         {
-            Assert.Equal(new StringCalculator().Add("//;\n2;3;5"), 10);
-            Assert.Equal(new StringCalculator().Add("//;\n2;3;5\n7"), 17);
+            Assert.Equal(StringCalculator.Add("//;\n2;3;5"), 10);
+            Assert.Equal(StringCalculator.Add("//;\n2;3;5\n7"), 17);
         }
         
         [Fact]
         public void sum_using_custom_delimiters_of_any_length()
         {
-            Assert.Equal(new StringCalculator().Add("//[***]\n2***3***5"), 10);
+            Assert.Equal(StringCalculator.Add("//[***]\n2***3***5"), 10);
         }
 
     }
