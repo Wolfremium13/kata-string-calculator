3218fb74a6910f299665ec6e15d3e6108405a773
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-19 10:44:51 +0000

    ✅ sum_using_custom_delimiters

--- kata-string-calculator/StringCalculator.cs
+++ kata-string-calculator/StringCalculator.cs
@@ -7,11 +7,20 @@ namespace kata_string_calculator
         public int Add(string numbers)
         {
             if (numbers.Length == 0) return 0;
+            if (numbers.StartsWith("//"))
+            {
+                var delimiter = numbers[2];
+                var numbersWithoutDelimiter = numbers.Substring(4);
+                var splitNumbers = numbersWithoutDelimiter.Split(delimiter);
+                return splitNumbers.Aggregate(0, (current, splitNumber) => current + int.Parse(splitNumber));
+            }
+
             if (numbers.Contains(",") || numbers.Contains("\n"))
             {
                 var splitNumbers = numbers.Split(',', '\n');
                 return splitNumbers.Aggregate(0, (current, splitNumber) => current + int.Parse(splitNumber));
             }
+
             return int.Parse(numbers);
         }
     }
